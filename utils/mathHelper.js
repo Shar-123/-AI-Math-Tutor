7d48564bbe5094e77042c2a456a67203a217a952
Shar-123 <lovelysharif.s.2006@gmail.com>
2026-03-08 15:55:44 +0530

    Delete utils directory

--- utils/mathHelper.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  solve: function(question) {
-    try {
-      return eval(question); // simple eval for math expressions
-    } catch (e) {
-      return "Invalid question";
-    }
-  }
-}
